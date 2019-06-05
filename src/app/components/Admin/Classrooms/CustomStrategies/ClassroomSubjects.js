5c2ee16566f832c93782fd46b1da248298cf4322
mahmoudafifi414 <pro.afifi100@gmail.com>
2019-06-05 16:43:41 +0200

    push the subject classrooms

--- src/app/components/Admin/Classrooms/CustomStrategies/ClassroomSubjects.js
+++ src/app/components/Admin/Classrooms/CustomStrategies/ClassroomSubjects.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
-class ClassroomSubjects extends Component{
-   render() {
-       return(<h1>Hello subjects</h1>);
-   }
+class ClassroomSubjects extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Subjects</h1>
+                <div className="col-md-2 form-group">
+                    <select className="form-control">
+                        <option>First</option>
+                    </select>
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default ClassroomSubjects
