9c41a09949aa7a27a517c63bec27159b6ae769fd
mahmoudafifi414 <pro.afifi100@gmail.com>
2019-07-16 09:59:05 +0200

    add front end react subject teacher tracking of adding teacher or remove it from subject

--- src/app/components/Admin/Subjects/SubjectTeacher.js
+++ src/app/components/Admin/Subjects/SubjectTeacher.js
@@ -7,11 +7,12 @@ import AddEditSubject from "./AddEditSubject";
 import {compareArrayDiffGeneric} from "../../../Helpers";
 
 class SubjectTeacher extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             showModal: false,
-            relatedTeachers: []
+            relatedTeachersStaging: [],
+            availableTeachersStaging: [],
         }
     }
 
@@ -24,6 +25,7 @@ class SubjectTeacher extends Component {
         this.setState({showModal: false});
     };
     handleSubjectSelection = (e) => {
+        this.setState({relatedTeachersStaging: [], availableTeachersStaging: []});
         const subject = e.currentTarget.value;
         this.props.getRelatedTeachers(subject);
     };
@@ -32,75 +34,123 @@ class SubjectTeacher extends Component {
             showModal: true,
         });
     };
-    attacheTeacherToSubject = (e) => {
-        const {relatedTeachers} = this.props.SubjectReducer;
-        this.setState({relatedTeachers: relatedTeachers});
+    attacheTeacherFromSubject = (e) => {
+        const id = e.currentTarget.id;
+        const selectedTeacher = this.props.UserReducer.allTeachers.filter(teacher => teacher.id == id);
+        this.setState(prevState => ({
+            relatedTeachersStaging: [
+                ...prevState.relatedTeachersStaging,
+                selectedTeacher[0]
+            ]
+        }));
+        this.setState(prevState => ({
+            availableTeachersStaging: [
+                ...prevState.availableTeachersStaging.filter(teacher => teacher.id != id)
+            ]
+        }));
+    };
+    detachTeacherFromSubject = (e) => {
+        const id = e.currentTarget.id;
+        const selectedTeacher = this.props.UserReducer.allTeachers.filter(teacher => teacher.id == id);
+        this.setState(prevState => ({
+            availableTeachersStaging: [
+                ...prevState.availableTeachersStaging,
+                selectedTeacher[0]
+            ]
+        }));
+        this.setState(prevState => ({
+            relatedTeachersStaging: [
+                ...prevState.relatedTeachersStaging.filter(teacher => teacher.id != id)
+            ]
+        }));
+    };
+    applyTeachersToSubject = (e) => {
+
     };
 
     render() {
         const {allTeachers} = this.props.UserReducer;
         const {subjects} = this.props.SubjectReducer;
-        const {relatedTeachers} = this.props.SubjectReducer;
-        /*console.log(relatedTeachers);*/
+        let relatedTeachers = [...this.props.SubjectReducer.relatedTeachers, ...this.state.relatedTeachersStaging];
+        relatedTeachers = relatedTeachers.filter(compareArrayDiffGeneric(this.state.availableTeachersStaging));
         const availableTeachers = allTeachers.filter(compareArrayDiffGeneric(relatedTeachers));
         return (
-            <div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <label htmlFor="year_name">Subject Name</label>
-                                <select id="subject_name" name="subject_name"
-                                        className="form-control" onChange={this.handleSubjectSelection}>
-                                    {subjects.map((subject, index) => (
-                                        <option key={subject.id} value={subject.id}>
-                                            {subject.name}
-                                        </option>
-                                    ))}
-                                </select>
-                                <button onClick={this.handleShow}>rapid add</button>
-                                <div className="list-group">
-                                    <h3 className="list-group-item list-group-item-action active justify-content-between">
-                                        Available Teachers
-                                    </h3>
-                                    {availableTeachers.length > 0 ?
-                                        availableTeachers.map((teacher, index) => (
-                                            <div key={teacher.id} className="list-group-item">
-                                                <button style={{float: 'right'}}
-                                                        className={"btn btn-default detachSubjectFromSemester " + teacher.id}
-                                                        id={teacher.id}
-                                                        onClick={this.attacheTeacherToSubject}>
-                                                    <span className="glyphicon glyphicon-plus"></span>
-                                                </button>
-                                                <h4 className="list-group-item-heading">
-                                                    {index + 1} - {teacher.name}
-                                                </h4>
-                                            </div>
-                                        )) : <div style={{textAlign: 'center'}} className="list-group-item">There are no
-                                            available Teachers</div>}
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-8">
+                                    <label htmlFor="subject_name">Subject Name</label>
+                                    <select id="subject_name" name="subject_name"
+                                            className="form-control" onChange={this.handleSubjectSelection}>
+                                        <option></option>
+                                        {subjects.map((subject, index) => (
+                                            <option key={subject.id} value={subject.id}>
+                                                {subject.name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className="col-md-1">
+                                    <button style={{'marginTop': 24}} className={"btn btn-primary"}
+                                            onClick={this.applyTeachersToSubject}>
+                                        <span className="glyphicon glyphicon-plus"></span>
+                                    </button>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={{'marginTop': 24}} className={"btn btn-primary"}
+                                            onClick={this.handleShow}>
+                                        Save Changes
+                                    </button>
                                 </div>
                             </div>
-                            <div className="col-md-6">
-                                <div className="list-group">
-                                    <h3 className="list-group-item list-group-item-action active justify-content-between">
-                                        Assigned Teachers
-                                    </h3>
-                                    {relatedTeachers.length > 0 ?
-                                        relatedTeachers.map((teacher, index) => (
-                                            <div key={teacher.id} className="list-group-item">
-                                                <button style={{float: 'right'}}
-                                                        className={"btn btn-danger detachSubjectFromSemester " + teacher.id}
-                                                        id={teacher.id}
-                                                        onClick={this.detachSubjectFromSemester}>
-                                                    <span className="glyphicon glyphicon-minus"></span>
-                                                </button>
-                                                <h4 className="list-group-item-heading">
-                                                    {index + 1} - {teacher.name}
-                                                </h4>
-                                            </div>
-                                        )) : <div style={{textAlign: 'center'}} className="list-group-item">List Is
-                                            Empty</div>}
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="list-group">
+                                        <h3 className="list-group-item list-group-item-action active justify-content-between">
+                                            Available Teachers
+                                        </h3>
+                                        {availableTeachers.length > 0 ?
+                                            availableTeachers.map((teacher, index) => (
+                                                <div key={teacher.id} className="list-group-item">
+                                                    <button style={{float: 'right'}}
+                                                            className={"btn btn-default detachSubjectFromSemester " + teacher.id}
+                                                            id={teacher.id}
+                                                            onClick={this.attacheTeacherFromSubject}>
+                                                        <span className="glyphicon glyphicon-plus"></span>
+                                                    </button>
+                                                    <h4 className="list-group-item-heading">
+                                                        {index + 1} - {teacher.name}
+                                                    </h4>
+                                                </div>
+                                            )) :
+                                            <div style={{textAlign: 'center'}} className="list-group-item">There are no
+                                                available Teachers</div>}
+                                    </div>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="list-group">
+                                <h3 className="list-group-item list-group-item-action active justify-content-between">
+                                    Assigned Teachers
+                                </h3>
+                                {relatedTeachers.length > 0 ?
+                                    relatedTeachers.map((teacher, index) => (
+                                        <div key={teacher.id} className="list-group-item">
+                                            <button style={{float: 'right'}}
+                                                    className={"btn btn-danger detachSubjectFromSemester " + teacher.id}
+                                                    id={teacher.id}
+                                                    onClick={this.detachTeacherFromSubject}>
+                                                <span className="glyphicon glyphicon-minus"></span>
+                                            </button>
+                                            <h4 className="list-group-item-heading">
+                                                {index + 1} - {teacher.name}
+                                            </h4>
+                                        </div>
+                                    )) : <div style={{textAlign: 'center'}} className="list-group-item">List Is
+                                        Empty</div>}
                             </div>
                         </div>
                     </div>
